cb8aee620b1a18c3810b238494dd385e3d803cbf
SangMin Park <psm9733@naver.com>
2018-03-18 23:48:47 +0900

    Add : loading.gif

---
+++ js/out.js
@@ -0,0 +1,13 @@
+function getImage(){
+  var ts = new Date().getTime();
+  document.getElementById("out_img").innerHTML ="<img src='images/origin.jpg?timestamp=" + ts + "'/>";
+};
+
+//main
+document.addEventListener("DOMContentLoaded", function(){
+  getImage();
+});
+
+// $(window).load(function() {
+//     $('#load').hide();
+// );
